14b7703a87ae8a92fa60a4272fe4a292abfe6dc7
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-11 21:56:30 +0300

    create models.py and add the source class

---
+++ app/models.py
@@ -0,0 +1,11 @@
+Class Source:
+    """
+        Source class that defines a news source objects.
+    """
+    def __init__(self, id, name, description):
+        """
+            Function that creates instances of the source objects.
+        """
+        self.id = id
+        self.name = name
+        self.description = description
