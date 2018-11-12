02b77a54a2e36bb8adf377606fac5f56f4a38efb
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-12 07:46:24 +0300

    add a class for the news articles

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-Class Source:
+class Source:
     """
         Source class that defines a news source objects.
     """
@@ -9,3 +9,17 @@ Class Source:
         self.id = id
         self.name = name
         self.description = description
+
+class Article:
+    """
+        Article class that defines article objects.
+    """
+    def __init__(self, title, image, description, time, url ):
+        """
+            Function that creates instances of the article objects.
+        """
+        self.title = title
+        self.image = image
+        self.description = description
+        self.time = time
+        self.url = url
