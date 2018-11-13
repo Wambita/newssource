a69b751a8729f0ded4e75443ddd580a0df245a0a
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-13 08:12:39 +0300

    remove base url from config class

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config:
     """
     General configuration parent class
     """
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
