c219e0717f6b847beb5430ef5bd916f276ad418e
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-13 08:16:48 +0300

    add category to source class

--- app/models.py
+++ app/models.py
@@ -2,22 +2,24 @@ class Source:
     """
         Source class that defines a news source objects.
     """
-    def __init__(self, id, name, description):
+    def __init__(self, id, name, category, description):
         """
             Function that creates instances of the source objects.
         """
         self.id = id
         self.name = name
+        self.category = category
         self.description = description
 
 class Article:
     """
         Article class that defines article objects.
     """
-    def __init__(self, title, image, description, time, url ):
+    def __init__(self, author, title, image, description, time, url ):
         """
             Function that creates instances of the article objects.
         """
+        self.author = author
         self.title = title
         self.image = image
         self.description = description
